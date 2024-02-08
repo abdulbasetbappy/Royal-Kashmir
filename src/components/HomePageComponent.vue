a52e24a623b32831664c827b2d6d1e0f98869d72
abdulbasetbappy <abdulbasetbappy@hotmial.com>
2024-02-08 23:22:05 +0600

    Hero Section Done

--- src/components/HomePageComponent.vue
+++ src/components/HomePageComponent.vue
@@ -1,103 +1,107 @@
-<template><!-- HomePageComponent.vue 
-    <div class='container'>
-        <div class="container__hero">
-            <div class="container__hero__leftside">
-                <h1>Get Your 
-                    <br>Healthy Food Now.</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, tempore explicabo nulla ad sint veritatis aspernatur ipsam molestias dolore quibusdam assumenda repellendus nam velit sed.</p>
-                <button>Delivery</button>
-                <button>Click & Collect</button>
-                <input type="text">
-            </div>
-            <div class="container__hero__rightside">
-                <div class="colordiv"></div>
-                <img src="../assets/hero.png" alt="Hero Image">
-            </div>
-        </div>
-        <div class="containerDown">
-        <div class="containerDown__heroItems">
-            <div class="containerDown__heroItems__Item1">
-                <img src="../assets/Herocart1.png" alt="Hero Item 1">
-                <h2>Healthy Food</h2>
-                <span>
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                </span>
-                <div>
-                    <p class="category">Salad</p>
-                <p class="price"><span>$14.50</span>$12.99</p>
-                </div>
-
-            </div>
-            <div class="containerDown__heroItems__Item2">
-                <img src="../assets/Herocart2.png" alt="Hero Item 1">
-                <h2>Diet Food</h2>
-                <span>
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                </span>
-                <div>
-                    <p class="category">Diet</p>
-                <p class="price"><span>$14.50</span>$12.99</p>
-                </div>
-
-            </div>
-            <div class="containerDown__heroItems__Item3">
-                <img src="../assets/Herocart3.png" alt="Hero Item 1">
-                <h2>Fried Riz</h2>
-                <span>
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                    <font-awesome-icon :icon="['fas', 'star']" />
-                </span>
-                <div>
-                    <p class="category">Riz Bowl</p>
-                <p class="price"><span>$14.50</span>$12.99</p>
-                </div>
-
-            </div>
-        </div>
+<template>
+<div class="container1">
+  <div class="container1__firstrow">
+    <div class="container1__firstrow__left">
+      <div class="container1__firstrow__left__title">
+        <h1>Lorem  amet <br> adipisicing elit.</h1>
+      </div>
+      <div class="container1__firstrow__left__description">
+        <p>Get the best deals on Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati inventore molestiae ex earum veniam cumque, quaerat dolorem quibusdam provident libero! the latest products</p>
+      </div>
+      <div class="container1__firstrow__left__button">
+        <button>Delivery</button>
+        <button>Pickup</button>
+      </div>
+      <div class="container1__firstrow__left__selection">
+        <input type="text" placeholder="Select Location" />
+      </div>
     </div>
+    <div class="container1__firstrow__right">
+      <img src="../assets/hero.png" alt="hero-image" />
     </div>
-    <div class="container2">
-        <div class="section1">
-            <img src="../assets/delivery-bike.png" alt="">
-            <div>
-                <h2>Fast Delivery</h2>
-            <p>With in 15 Minutes</p>
-            </div>
-        </div>
-        <div class="section2">
-            <img src="../assets/house.png" alt="">
-            <div>
-                <h2>Healthy Food</h2>
-            <p>Organic & Home Made</p>
-            </div>
-        </div>
-        <div class="section3">
-            <img src="../assets/booking.png" alt="">
-            <div>
-                <h2>Reservation</h2>
-            <p>Quite & Natural Place</p>
-            </div>
-        </div>
-    </div>-->
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae odio ratione quod eum rem beatae assumenda nam fugiat cupiditate saepe praesentium labore, enim consectetur veniam voluptas quia aspernatur! Voluptate sit iure voluptates reprehenderit repudiandae officiis nobis ex veritatis, nesciunt distinctio nostrum repellendus molestias commodi libero accusantium harum laudantium in doloremque exercitationem maxime nihil dicta. Inventore consequatur excepturi facilis atque, non corrupti ipsum recusandae doloremque deserunt magnam animi! Minima odit nesciunt dolorem, perspiciatis blanditiis laudantium animi aliquam labore ipsum quo eligendi, aperiam quasi saepe dolore. Voluptate velit ipsam ratione suscipit iure nihil, esse nemo, quae perspiciatis dolorem deserunt deleniti quo, optio fugiat! Placeat, magnam voluptas! Laudantium adipisci inventore nisi voluptas quisquam pariatur provident dolore illo, perferendis minus, eos necessitatibus iste sapiente consequuntur neque quaerat? Delectus debitis molestiae nihil culpa iure ullam laborum omnis fugiat. Voluptatem, maxime vero obcaecati velit aperiam pariatur cumque sint unde! Sed aliquam facere, assumenda eaque dignissimos repellat atque nisi libero cupiditate praesentium nulla necessitatibus accusamus. Qui quo odit temporibus adipisci dicta aliquid accusantium vel quidem, ex doloremque! Vel accusamus, placeat ad ex blanditiis dolorum excepturi suscipit cum debitis dolores hic in inventore asperiores natus perspiciatis distinctio quaerat quo ab commodi accusantium officia magni animi mollitia. Pariatur, qui.
+  </div>
+  <div class="container1__secondrow">
+    <div class="container1__secondrow__Item1">
+      <img src="../assets/Herocart1.png" alt="">
+      <h3>Salade Bowl</h3>
+      <div class="stars">
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+      </div>
+      <div class="details">
+        <p>Salade</p>
+        <h4><span>$12.99</span>$10.99</h4>
+      </div>
+    </div>
+    <div class="container1__secondrow__Item2">
+      <img src="../assets/Herocart2.png" alt="">
+      <h3>Salade Bowl</h3>
+      <div class="stars">
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+      </div>
+      <div class="details">
+        <p>Salade</p>
+        <h4><span>$12.99</span>$10.99</h4>
+      </div>
+    </div>
+    <div class="container1__secondrow__Item3">
+      <img src="../assets/Herocart3.png" alt="">
+      <h3>Salade Bowl</h3>
+      <div class="stars">
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+        <font-awesome-icon class="star" :icon="['fas', 'star']" />
+      </div>
+      <div class="details">
+        <p>Salade</p>
+        <h4><span>$12.99</span>$10.99</h4>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container2">
+  <div class="container2__section1">
+    <div class="s_details">
+      <img src="../assets/delivery-bike.png" alt="">
+      <div class="text_section">
+        <h1>Fastest Delivery</h1>
+        <p>Safe & Fastest Delivery</p>
+      </div>
+    </div>
+  </div>
+  <div class="container2__section2">
+    <div class="s_details">
+      <img src="../assets/booking.png" alt="">
+      <div class="text_section">
+        <h1>Book Now</h1>
+        <p>Natural Place</p>
+      </div>
+    </div>
+  </div>
+  <div class="container2__section3">
+    <div class="s_details">
+      <img src="../assets/house.png" alt="">
+      <div class="text_section">
+        <h1>Home Made</h1>
+        <p>Fresh Felling Ever</p>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
-<script setup lang="ts">
-
+<script lang="ts" setup>
 </script>
 
 <style lang="scss" scoped>
-@import '../styles/Pages/homePage.scss';
-
+@import '../styles/Pages/homePageComponent';
 </style>
